4e04b64676ad6a0ee1d381089ad9bfced48c9e02
Sarah <sarah.ebel@outlook.com>
2016-06-08 16:41:08 +0200

    ImageUpload
    - remove check, if title exists in db (not working)
    - add upload cover image to "img" directory, write imgURL in database

--- pages/addentry.php
+++ pages/addentry.php
@@ -1,45 +1,49 @@
 <?php
 // -------------------------------------------------------------------------- //
 // -------------------------------------------------------------------------- //
-// NEW ARRAY FOR NEW ENTRY //
-$titleexists=$_POST['filmtitle'];
-
-// -------------------------------------------------------------------------- //
 // CONNECT TO DATABASE //
 $database = new PDO('mysql:host=localhost;dbname=mediadb', 'root', '');
 
 // -------------------------------------------------------------------------- //
-// CHECK IF MOVIE EXISTS IN DATABASE
-$result = $database->query("SELECT title FROM movies WHERE title = '$titleexists'");
+// SAVE UPLOADED IMAGE TO IMG DIRECTORY //
+$uploaddir = $_SERVER['DOCUMENT_ROOT'] . "/mediaDB/img/cover/";
+$uploadfile = $uploaddir . basename($_FILES['cover']['name']);
 
-if($result->fetchColumn() > 0) {
-    
-    // NEW ARRAY FOR NEW ENTRY //
-    $arraynewentry = array();
-    $arraynewentry['title']=$_POST['filmtitle'];
-    $arraynewentry['origtitle']=$_POST['origtitle'];
-    $arraynewentry['cover']=$_POST['cover'];
-    $arraynewentry['date']=$_POST['date'];
-    $arraynewentry['country']=$_POST['country'];
-    $arraynewentry['length']=$_POST['length'];
-    $arraynewentry['fsk']=$_POST['fsk'];
-    $arraynewentry['genre']=$_POST['genre'];
-    $arraynewentry['actors']=$_POST['actors'];
-    $arraynewentry['director']=$_POST['director'];
-    $arraynewentry['summery']=$_POST['summery'];
-    $arraynewentry['location']=$_POST['location']; 
-    
-    // WRITE NEW ENTRY IN DATABASE //
-    $newentry = $database->prepare("INSERT INTO movies (title, origtitle, cover, date, country, length, fsk, genre, actors, director, summery, location) VALUES (:title, :origtitle, :cover, :date, :country, :length, :fsk, :genre, :actors, :director, :summery, :location)");
-    $newentry->execute($arraynewentry);
-    
-    // SHOW ENTRY SUCCEED + REDIRECT TO INDEX.HTML AFTER 5 SEC. //
-    include('entrysucceed.html');
-    header( "refresh:3;url=../index.php" );
+// MOVE IMG TO IMAGE DIRECTORY //
+if (move_uploaded_file($_FILES['cover']['tmp_name'], $uploadfile)) {
+  
 } else {
-    
-    // SHOW ENTRY FAILED + REDIRECT TO INDEX.HTML AFTER 5 SEC. //
-    include('entryfailed.html');
-    header( "refresh:3;url=../index.php" );
+   echo "Upload failed";
 }
+
+// SET IMAGE URL //
+$imgurl=$uploaddir.$_FILES['cover']['name']; 
+
+// ARRAY FOR NEW ENTRY //
+$arraynewentry = array();
+$arraynewentry['title']=$_POST['filmtitle'];
+$arraynewentry['origtitle']=$_POST['origtitle'];
+$arraynewentry['cover']=$imgurl;
+$arraynewentry['date']=$_POST['date'];
+$arraynewentry['country']=$_POST['country'];
+$arraynewentry['length']=$_POST['length'];
+$arraynewentry['fsk']=$_POST['fsk'];
+$arraynewentry['genre']=$_POST['genre'];
+$arraynewentry['actors']=$_POST['actors'];
+$arraynewentry['director']=$_POST['director'];
+$arraynewentry['summery']=$_POST['summery'];
+$arraynewentry['location']=$_POST['location']; 
+    
+// WRITE NEW ENTRY IN DATABASE //
+$newentry = $database->prepare("INSERT INTO movies (title, origtitle, cover, date, country, length, fsk, genre, actors, director, summery, location) VALUES (:title, :origtitle, :cover, :date, :country, :length, :fsk, :genre, :actors, :director, :summery, :location)");
+$newentry->execute($arraynewentry);
+
+// SHOW ENTRY SUCCEED + REDIRECT TO INDEX.HTML AFTER 5 SEC. //
+include('entrysucceed.html');
+header( "refresh:3;url=../index.php" );
+    
+// SHOW ENTRY FAILED + REDIRECT TO INDEX.HTML AFTER 5 SEC. //
+/*include('entryfailed.html');
+header( "refresh:3;url=../index.php" );*/
+
 ?>
