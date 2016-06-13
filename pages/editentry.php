1567e615698efa5bdff98c3f8f179fdb3f42b8ff
Sarah <ebelshei@iese.fhg.de>
2016-06-13 11:08:51 +0200

    Add editentry.php
    add php file to edite movie entries

---
+++ pages/editentry.php
@@ -0,0 +1,15 @@
+<?php
+/*********************************************************************
+Modulename: editentry.php
+Project:    mediaDB
+Author:     Sarah Ebelsheiser <sarah.ebel@outlook.com>
+
+add movie entry to database
+
+Version 1.0.0
+2016-06-13
+**********************************************************************/
+
+SESSION_START(); 
+    echo "Edit Entry";
+?>
