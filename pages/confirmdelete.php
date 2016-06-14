99117544c36bb697d319f573ca0cdf8fb66ba711
Sarah <ebelshei@iese.fhg.de>
2016-06-14 14:05:56 +0200

    Edit confirmdelete.php
    - Design according other message pages

--- pages/confirmdelete.php
+++ pages/confirmdelete.php
@@ -10,10 +10,38 @@ Version 1.0.0
 2016-06-14
 **********************************************************************/
 
-echo "Are you sure you want to delete:
-<br>";
+include("header.html");
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+    <title>EMDB</title>
+    <link rel="stylesheet" type="text/css" href="../styles/header.css" />    
+    <link rel="stylesheet" type="text/css" href="../styles/message.css" />
+	</head>
+</html>
+
+<?php
+
+// -------------------------------------------------------------------------- //
+// CONNECT TO DATABASE //
+$database = new PDO('mysql:host=localhost;dbname=mediadb', 'root', '');
+
+// -------------------------------------------------------------------------- //
+// GET TITLE //
+$sql = $database->query("SELECT title FROM movies WHERE id = '$_GET[id]'");
+$result = $sql->fetch(\PDO::FETCH_ASSOC);
+$titel=$result['title'];
+
+
+// -------------------------------------------------------------------------- //
+// DIV CONFIRM DELETE //
 echo "
-<a href=deleteentry.php?confirm=yes&id=$_GET[id]>Yes</a>
-<a href=deleteentry.php?confirm=no&id=$_GET[id]>No</a>";
+<div class='div confirmdelete'>
+<label>Möchten Sie den folgenden Titel entgültig löschen?<br><br><strong> $titel </strong><br></label><br><br>
+
+<a class='button yes' href=deleteentry.php?confirm=yes&id=$_GET[id]>Yes</a>
+<a class='button no' href=deleteentry.php?confirm=no&id=$_GET[id]>No</a>";
 
 ?>
