5b247ef14506ed4512fe9909e32ffb480942249d
Sarah <ebelshei@iese.fhg.de>
2016-06-14 16:07:45 +0200

    Edit deleteentry.php
    - redirect to index.php
    - delete cover image if one exists

--- pages/deleteentry.php
+++ pages/deleteentry.php
@@ -18,6 +18,17 @@ $id = $_GET['id'];
 $database = new PDO('mysql:host=localhost;dbname=mediadb', 'root', '');
 
 // -------------------------------------------------------------------------- //
+// DELETE COVER IMAGE IF EXIST //
+$sql = $database->query("SELECT cover FROM movies WHERE id = '$_GET[id]'");
+$result = $sql->fetch(\PDO::FETCH_ASSOC);
+$cover=$result['cover'];
+if (!file_exists("../$cover")) {
+        
+} else {
+unlink("../$cover");
+}
+
+// -------------------------------------------------------------------------- //
 // DELETE ENTRY WITH SELECTED ID //
 if ( $_GET['confirm'] == "yes" ) {
     $sql = "DELETE FROM movies WHERE id='$id'";
@@ -27,6 +38,10 @@ if ( $_GET['confirm'] == "yes" ) {
     // SHOW ENTRY SUCCEED + REDIRECT TO INDEX.HTML AFTER 3 SEC. //
     header( "location: ../index.php");
 }
+
+// -------------------------------------------------------------------------- //
+// SHOW DELETE SUCCEED + REDIRECT TO INDEX.HTML AFTER 5 SEC. //
+include('deletesucceed.html');
 
 
 ?>
