fde5aeaa410a941bb67307d192e257faa989b331
Sarah <ebelshei@iese.fhg.de>
2016-06-15 16:01:06 +0200

    Add updateentry.php
    file to update movie details in database (details come from formular
    formupdateentry.php

---
+++ pages/updateentry.php
@@ -0,0 +1,13 @@
+<?php
+/*********************************************************************
+Modulename: updateentry.php
+Project:    mediaDB
+Author:     Sarah Ebelsheiser <sarah.ebel@outlook.com>
+
+update movie details in database
+
+Version 1.0.0
+2016-06-15
+**********************************************************************/
+
+?>
