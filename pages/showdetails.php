9d01fd05e4758139b2fcce0b80c2e8a7cf9d0a0e
Sarah <ebelshei@iese.fhg.de>
2016-06-16 15:40:45 +0200

    Edit showdetails.php
    - change comments
    - change style sheet to "details.css"
    - div "details" from php -> html

--- pages/showdetails.php
+++ pages/showdetails.php
@@ -4,10 +4,10 @@ Modulename: showdetails.php
 Project:    mediaDB
 Author:     Sarah Ebelsheiser <sarah.ebel@outlook.com>
 
-add movie entry to database
+show movie details
 
 Version 1.0.0
-2016-06-15
+2016-06-16
 **********************************************************************/
 
 include("header.html");
@@ -18,7 +18,7 @@ include("header.html");
 	<head>
     <title>EMDB</title>
     <link rel="stylesheet" type="text/css" href="../styles/header.css" />    
-    <link rel="stylesheet" type="text/css" href="../styles/message.css" />
+    <link rel="stylesheet" type="text/css" href="../styles/details.css" />
 	</head>
 </html>
 
@@ -45,22 +45,32 @@ $director=$details['director'];
 $summery=$details['summery'];
 $location=$details['location'];
 
-// -------------------------------------------------------------------------- //
-// DIV DETAILS //
-echo "
-<div class='div details'>
-<label><strong> $titel </strong><br></label><br><br>
-$origtitle <br>
-<img style='border-width: 0px;' src='../$coverimg' width='80' height='114'/><br>
-Erscheinungsjahr:$date<br>
-Produktionsland:$country<br>
-Filmlänge:$length<br>
-Altersfreigabe:$fsk<br>
-Genre:$genre<br>
-Darsteller:$actors<br>
-Regisseur:$director<br>
-Zusammenfassung:$summery<br>
-Standort/Speicherort:$location<br>
-</div>"
-
 ?>
+
+<html>
+    <body>
+        <!-- -------------------------------------------------------------------------- -->
+        <!-- DIV WRAPPER FOR DETAILS OF MOVIE -->
+        <div class="div-wrapper">
+            <!-- DIV FOR BUTTON BACK -->
+            <div class="div-buttonback">
+                <a class='button back' href='../index.php'>zur&uuml;ck zur Hauptseite</a>
+            </div>
+            <!-- DIV FOR MOVIE DETAILS -->
+            <div class="div-details">
+                <h2><strong><?php echo"$titel"?></strong></h2>
+                <p><?php echo"$origtitle"?></p><br>
+                <?php echo "<img style='border-width: 0px;' src='../$coverimg' width='80' height='114'/><br>"?><br>
+                <p>Erscheinungsjahr: <?php echo"$date"?></p>
+                <p>Produktionsland: <?php echo"$country"?></p>
+                <p>Filmlänge: <?php echo"$length"?></p>
+                <p>Altersfreigabe (FSK): <?php echo"$fsk"?></p>
+                <p>Genre: <?php echo"$genre"?></p>
+                <p>Darsteller: <?php echo"$actors"?></p>
+                <p>Regisseur: <?php echo"$director"?></p>
+                <p>Zusammenfassung: <?php echo"$summery"?></p>
+                <p>Standort / Speicherort: <?php echo"$location"?></p>
+            </div>
+        </div>
+    </body>
+</html>
