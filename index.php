ba4d4f635660d216fd0fd00f815beb0510ce9033
Sarah <ebelshei@iese.fhg.de>
2016-06-22 16:50:27 +0200

    Update index.php
    - define variables to connect to database
    - beatutify code

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ Modulename: index.php
 Project:    mediaDB
 Author:     Sarah Ebelsheiser <sarah.ebel@outlook.com>
 
-main page of mediaDB
+main page of mediaDB - show all database entries
 
 Version 1.0.0
-2016-06-20
+2016-06-22
 **********************************************************************/
 
 include ("pages/header.html");
@@ -16,33 +16,23 @@ include ("pages/header.html");
 <!DOCTYPE html>
 <html>
 	<head>
-    <title>EMDB</title>
     <link rel="stylesheet" type="text/css" href="css/header.css" />
     <link rel="stylesheet" type="text/css" href="css/index.css" />
 	</head>
-    
-	<body>
-
-<!-- -------------------------------------------------------------------------- -->
-<!-- BUTTONS -->
-        <div class="div-wrapper-buttons">
-            <div class="div-new-entry">
-                <a class="button btnnewentry" href="pages/formnewentry.php">Eintrag hinzuf&uuml;gen</a>
-            </div>
-        </div>
-        <hr> 
-        
-	</body>
 </html>
 
 
-<!-- -------------------------------------------------------------------------- -->
-<!-- SHOW DATABASE ENTRIES -->
 <?php
+// -------------------------------------------------------------------------- //
+// DATABASE CONNECTION VARIABLES //
+$dbhost='localhost';
+$dbname='mediadb';
+$dbusername='root';
+$dbpw='';
 
 // -------------------------------------------------------------------------- //
 // CONNECT TO DATABASE //
-$database = new PDO('mysql:host=localhost;dbname=mediadb', 'root', '');
+$database = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpw);
 
 // -------------------------------------------------------------------------- //
 // SQLQUERY ORDER BY TITLE //
@@ -54,11 +44,11 @@ $sql= 'SELECT * FROM movies ORDER BY title';
 echo "<div class='div-wrapper-movies'>";
 
 // -------------------------------------------------------------------------- //
-// DISPLAY ENTRIES OF DATABASE IN COVER STYLE //
+// DISPLAY ALL ENTRIES OF DATABASE IN COVER STYLE //
 foreach ($database->query($sql) as $row)
   {
     $img=$row['cover'];
-    $shorttitle=substr($row['title'],0,24);
+    $shorttitle=substr($row['title'],0,24); // CUT TITLE AFTER 24 CHARACTERS
     echo "<div class='div-cover'>
             <a href=pages/details.php?id=$row[id]><img src='$img'/></a>
             <span class='title'>$shorttitle</span><br>
