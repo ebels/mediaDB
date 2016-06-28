d0341751c4ec9c075046a79e4d47e27c3db7ea09
Sarah <ebelshei@iese.fhg.de>
2016-06-28 16:20:45 +0200

    Beautify code in confirm.php

--- pages/confirm.php
+++ pages/confirm.php
@@ -7,28 +7,11 @@ Author:     Sarah Ebelsheiser <sarah.ebel@outlook.com>
 yes / no confirmation to delete movie
 
 Version 1.0.0
-2016-06-23
+2016-06-28
 **********************************************************************/
 
 include("header.html");
-?>
 
-<!DOCTYPE html>
-<html>
-	<head>
-        <link rel="stylesheet" type="text/css" href="../css/header.css" />    
-        <link rel="stylesheet" type="text/css" href="../css/message.css" />
-        <style>
-            .img-cover {
-                width: 182px;
-                height: 268px;
-                border-radius: 10px;
-            }
-        </style>
-	</head>
-</html>
-
-<?php
 // -------------------------------------------------------------------------- //
 // DATABASE CONNECTION VARIABLES //
 $dbhost='localhost';
@@ -47,17 +30,35 @@ $result = $sql->fetch(\PDO::FETCH_ASSOC);
 $titel=$result['title'];
 $coverimg=$result['cover'];
 
-
-// -------------------------------------------------------------------------- //
-// DIV CONFIRM DELETE //
-echo "
-<div class='div confirmdelete'>
-<label>Möchten Sie den folgenden Titel entgültig löschen?<br><br>
-<hr><br>
-<strong> $titel </strong><br></label><br>
-<img class='img-cover' src='/mediaDB/$coverimg'/><br><br><hr><br>
-
-<a class='button yes' href=../modules/deleteentry.php?confirm=yes&id=$_GET[id]>Ja</a>
-<a class='button no' href=../modules/deleteentry.php?confirm=no&id=$_GET[id]>Nein</a>";
-
 ?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+        <link rel="stylesheet" type="text/css" href="../css/header.css" />    
+        <link rel="stylesheet" type="text/css" href="../css/message.css" />
+        <style>
+            .img-cover {
+                width: 182px;
+                height: 268px;
+                border-radius: 10px;
+            }
+        </style>
+	</head>
+    
+    <!-- -------------------------------------------------------------------------- -->
+    <!-- DIV CONFIRM DELETE -->
+    <div class="div confirmdelete">
+        <label>Möchten Sie den folgenden Titel entgültig löschen?<br><br>
+        <hr><br>
+        <strong> <?php echo "$titel" ?></strong><br></label><br>
+        <img class="img-cover" src="/mediaDB/<?php echo "$coverimg" ?>"/><br><br><hr><br>
+
+        <?php
+        echo "
+        <a class='button yes' href=../modules/deleteentry.php?confirm=yes&id=$_GET[id]>Ja</a>
+        <a class='button no' href=../modules/deleteentry.php?confirm=no&id=$_GET[id]>Nein</a>";
+        ?>
+    </div>
+</html>
