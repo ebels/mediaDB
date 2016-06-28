9f548e47b823753f69cdad9f0c9fdaaf42d2cb37
Sarah <ebelshei@iese.fhg.de>
2016-06-28 16:20:02 +0200

    covers in dynamic content div - beautify code

--- index.php
+++ index.php
@@ -7,22 +7,11 @@ Author:     Sarah Ebelsheiser <sarah.ebel@outlook.com>
 main page of mediaDB - show all database entries
 
 Version 1.0.0
-2016-06-23
+2016-06-28
 **********************************************************************/
-
 include ("pages/header.html");
-?>
-
-<!DOCTYPE html>
-<html>
-	<head>
-        <link rel="stylesheet" type="text/css" href="css/header.css" />
-        <link rel="stylesheet" type="text/css" href="css/index.css" />
-	</head>
-</html>
 
 
-<?php
 // -------------------------------------------------------------------------- //
 // DATABASE CONNECTION VARIABLES //
 $dbhost='localhost';
@@ -37,24 +26,42 @@ $database = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpw);
 // -------------------------------------------------------------------------- //
 // SQLQUERY ORDER BY TITLE //
 $sql= 'SELECT * FROM movies ORDER BY title';
-
-
-// -------------------------------------------------------------------------- //
-// DIV WRAPPER FOR ALL MOVIE COVERS //
-echo "<div class='div-wrapper-movies'>";
-
-// -------------------------------------------------------------------------- //
-// DISPLAY ALL ENTRIES OF DATABASE IN COVER STYLE //
-foreach ($database->query($sql) as $row)
-  {
-    $img=$row['cover']; // SET COVER IMAGE
-    $shorttitle=substr($row['title'],0,24); // CUT TITLE AFTER 24 CHARACTERS
-    echo "<div class='div-cover'>
-            <a href=pages/details.php?id=$row[id]><img src='$img'/></a>
-            <span class='title'>$shorttitle</span><br>
-            <span class='genre'>$row[genre]</span><br><br>
-    </div>";
-  }
-
-echo "</div>";
 ?>
+
+
+<!-- -------------------------------------------------------------------------- -->
+<!-- HTML PART -->
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/header.css" />
+        <link rel="stylesheet" type="text/css" href="css/index.css" />
+    </head>
+
+<!-- -------------------------------------------------------------------------- -->
+<!-- CONTAINER CONTENT -->
+<div class="div-container-content">
+   
+    <!-- -------------------------------------------------------------------------- -->
+    <!-- DIV WRAPPER FOR ALL MOVIE COVERS -->
+    <div class='div-wrapper-movies'>
+
+    <!-- -------------------------------------------------------------------------- -->
+    <!-- DISPLAY ALL ENTRIES OF DATABASE IN COVER STYLE -->
+    <?php
+        foreach ($database->query($sql) as $row)
+      {
+        $img=$row['cover']; // SET COVER IMAGE
+        $shorttitle=substr($row['title'],0,24); // CUT TITLE AFTER 24 CHARACTERS
+        echo "<div class='div-cover'>
+                <a href=pages/details.php?id=$row[id]><img src='$img'/></a>
+                <span class='title'>$shorttitle</span><br>
+                <span class='genre'>$row[genre]</span><br><br>
+        </div>";
+      } ?>
+
+    </div>
+    <br><br><br>
+    
+</div>
+</html>
