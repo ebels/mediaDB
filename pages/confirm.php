d3a14eb57d605e8855f25697004e348a1b392d37
Sarah <ebelshei@iese.fhg.de>
2016-06-30 16:16:45 +0200

    Adjust style to "about" page

--- pages/confirm.php
+++ pages/confirm.php
@@ -7,7 +7,7 @@ Author:     Sarah Ebelsheiser <sarah.ebel@outlook.com>
 yes / no confirmation to delete movie
 
 Version 1.0.0
-2016-06-28
+2016-06-30
 **********************************************************************/
 
 include("header.html");
@@ -38,27 +38,28 @@ $coverimg=$result['cover'];
 	<head>
         <link rel="stylesheet" type="text/css" href="../css/header.css" />    
         <link rel="stylesheet" type="text/css" href="../css/message.css" />
-        <style>
-            .img-cover {
-                width: 182px;
-                height: 268px;
-                border-radius: 10px;
-            }
-        </style>
 	</head>
+    <body>
     
     <!-- -------------------------------------------------------------------------- -->
     <!-- DIV CONFIRM DELETE -->
-    <div class="div confirmdelete">
-        <label>Möchten Sie den folgenden Titel entgültig löschen?<br><br>
-        <hr><br>
-        <strong> <?php echo "$titel" ?></strong><br></label><br>
-        <img class="img-cover" src="/mediaDB/<?php echo "$coverimg" ?>"/><br><br><hr><br>
-
-        <?php
-        echo "
-        <a class='button yes' href=../modules/deleteentry.php?confirm=yes&id=$_GET[id]>Ja</a>
-        <a class='button no' href=../modules/deleteentry.php?confirm=no&id=$_GET[id]>Nein</a>";
-        ?>
-    </div>
+    <div class="div-background"></div>
+        <div class="div message">
+            <div class="top-border left"></div>
+            <div class="top-border right"></div>
+            <h1><?php echo "$titel" ?></h1>
+            <p>
+                Möchten Sie den Film entgültig löschen?<br>
+            </p>
+            
+            <img class="img-cover" src="/mediaDB/<?php echo "$coverimg" ?>"/><br>   
+            <br><br>
+            
+            <?php 
+            echo "
+            <a class='button-yes' href=../modules/deleteentry.php?confirm=yes&id=$_GET[id]>Ja</a>
+            <a class='button-no' href=../modules/deleteentry.php?confirm=no&id=$_GET[id]>Nein</a>";
+            ?>
+        </div>
+    </body>
 </html>
