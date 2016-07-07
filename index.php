5df7d2ec403a2d81d47343edc5bf9fd99a9761d0
Sarah <sarah.ebel@outlook.com>
2016-07-07 16:18:14 +0200

    Insert break after each cover div

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ Author:     Sarah Ebelsheiser <sarah.ebel@outlook.com>
 main page of mediaDB - show all database entries
 
 Version 1.0.0
-2016-07-05
+2016-07-07
 **********************************************************************/
 include ("pages/header.html");
 require_once ("modules/dbconnect.php");
@@ -48,7 +48,7 @@ $sql= 'SELECT * FROM movies ORDER BY title';
                 $img=$row['cover']; // SET COVER IMAGE
                 $shorttitle=substr($row['title'],0,24); // CUT TITLE AFTER 24 CHARACTERS
                 echo "<div class='div-cover'>
-                        <a href=pages/details.php?id=$row[id]><img src='$img'/></a>
+                        <a href=pages/details.php?id=$row[id]><img src='$img'/></a><br>
                         <span class='title'>$shorttitle</span><br>
                         <span class='genre'>$row[genre]</span><br><br>
                 </div>";
